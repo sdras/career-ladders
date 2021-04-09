606485c74568ebcade47269485d141277aecf838
Sarah Drasner <sarah.drasner@gmail.com>
2021-04-09 09:16:57 -0600

    fix up the footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,12 +1,10 @@
 <template>
   <footer>
     <p>
-      This project is
-      <a href="https://github.com/sdras/career-ladders">open source on github</a
-      >, hosted with <a href="https://bit.ly/2G29YwK">Netlify</a>, and made with
-      🌮 by Sarah Drasner,
+      This project is made with 🌮 by Sarah Drasner,
       <a href="https://twitter.com/sarah_edo">@sarah_edo (twitter)</a> &
       <a href="https://github.com/sdras">@sdras (github)</a>
+      <br />Hosted with <a href="https://bit.ly/2G29YwK">Netlify</a>
     </p>
   </footer>
 </template>
@@ -19,5 +17,9 @@ a:visited,
 a:active {
   color: #c4c3ec;
   font-weight: bold;
+}
+
+footer {
+  font-size: 0.9rem;
 }
 </style>
