01de7f0745365f31d39adbece54911cc42abdd1d
Sarah Drasner <sarah.drasner@gmail.com>
2021-04-14 18:01:54 -0600

    edit the about text

--- components/AppAbout.vue
+++ components/AppAbout.vue
@@ -15,7 +15,7 @@
         ><a
           href="https://css-tricks.com/the-importance-of-career-laddering/"
           target="_blank"
-          >This writeup explains a bit more about that process ></a
+          >This writeup explains a bit more about my process ></a
         ></strong
       >
     </p>
@@ -25,16 +25,19 @@
       trust.
     </p>
     <p>
-      I'm open sourcing my career ladders so that people have a launching-off
-      point. I do not have this all figured out, please feel free to iterate, or
-      give feedback, or submit a PR. Full transparency, I don't have a ton of
-      time for maintenance right now, and am maintaining a
+      I'm open sourcing my career ladders so people have a launching-off point.
+      I do not have this all figured out, please feel free to iterate, or give
+      feedback, or submit a PR. Full transparency, I don't have a ton of time
+      for maintenance right now, and am maintaining a
       <span class="italic">lot</span> of repos, so if your PR goes unnoticed for
       a while, that's why. I also may reject a PR if it's not the way I
       personally want to structure something, but that's ok! It's alright for us
-      to have different ways of creating laddering, in fact, I would encourage
-      one to make some changes depending on your org in particular, we don't
-      have to all be the same, and probably shouldn't be.
+      to have different ways of creating laddering.
+    </p>
+    <p>
+      In fact, I would encourage one to make some changes depending on your org
+      in particular, we don't have to all be the same, and probably shouldn't
+      be.
     </p>
     <p>
       Special thanks to Lauren Sell, Dizzy Smith, Jason Lengstorf, and Dalia
